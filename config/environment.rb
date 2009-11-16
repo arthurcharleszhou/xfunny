4bb2fcf0057e29dea69aadde788799ade608f416
Tower He <tower@rails-dev.(none)>
2009-11-16 19:15:18 +0800

    delete test directory

--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,9 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
+  config.gem "authlogic", :lib => "authlogic", :version => "2.1.3"
+  config.gem "haml", :lib => "haml", :version => "2.2.13"
+
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
--- features/support/paths.rb
+++ features/support/paths.rb
@@ -10,6 +10,9 @@ module NavigationHelpers
     
     when /the home\s?page/
       '/'
+
+    when /the sign up page/
+      '/signup'
     
     # Add more mappings here.
     # Here is a more fancy example:
